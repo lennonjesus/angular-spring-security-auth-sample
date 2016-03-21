2504431e67b2f9045e958b39607e1a5e02aada29
Lennon Jesus <lennon.jesus@gmail.com>
2016-03-21 19:19:57 -0300

    now the front works well

--- api/src/main/groovy/com/lennonjesus/auth/rest/HelloController.groovy
+++ api/src/main/groovy/com/lennonjesus/auth/rest/HelloController.groovy
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
+import java.security.Principal
+
 /**
  * @author Lennon Jesus
  */
@@ -15,4 +17,16 @@ class HelloController {
         "Hello, authenticated user."
     }
 
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
 }
