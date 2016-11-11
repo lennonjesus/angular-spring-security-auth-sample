e4094741d388329e32efb40524c1c70947b9e049
Leandro Souza <patricia.goncalves@mprj.mp.br>
2016-11-11 18:26:20 -0200

    Configuracao karma

---
+++ frontend/test/karma.conf.js
@@ -0,0 +1,69 @@
+module.exports = function(config) {
+  config.set({
+        basePath: '../',
+        frameworks: ['jasmine'],
+
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angular-route,angular-route.js',
+            'bower_components/angular-ui-router/release/angular-ui-router.js',
+
+            /*
+            'bower_components/angular-animate/angular-animate.js',
+            'bower_components/angular-cookies/angular-cookies.js',
+            'bower_components/angular-http-auth/src/http-auth-interceptor.js',
+            'bower_components/angular-resource/angular-resource.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+            'bower_components/angular-translate/angular-translate.js',
+            'bower_components/angular-translate-loader-static-files/angular-translate-loader-static-files.js',
+            */
+            'test/unit/*.spec.js',
+            'app/hello.js'
+
+        ],
+
+        exclude: [
+
+        ],
+
+    // web server port
+        port: 9876,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_WARN,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+        browsers: [
+            'Chrome'
+        ],
+
+
+         // Which plugins to enable
+         plugins: [
+           'karma-chrome-launcher',
+           //'karma-phantomjs-launcher',
+           //'karma-coverage',
+           'karma-jasmine',
+           //'karma-jasmine-html-reporter'
+         ],
+
+
+         // Continuous Integration mode
+         // if true, Karma captures browsers, runs the tests and exits
+         singleRun: false,
+
+        plugins: [
+           'karma-chrome-launcher',
+           'karma-jasmine',
+           //'karma-jasmine-html-reporter'
+        ],
+
+  });
+};
