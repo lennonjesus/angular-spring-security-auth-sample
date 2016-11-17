41fcb41bc905e6fdf8a05c833ca18955ff9f9268
Lennon Jesus <lennon.jesus@gmail.com>
2016-11-17 15:07:54 -0200

    fix: adding missing dependencies and set phantomjs as default test browser
    - adding jasmine-core dependency
    - adding phantomjs as test browser
    - remove duplicated plugins section on karma.conf.js

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -1,54 +1,45 @@
 module.exports = function(config) {
   config.set({
-        basePath: '../',
-        frameworks: ['jasmine'],
+    basePath: '../',
+    frameworks: ['jasmine'],
 
-        files: [
-            'bower_components/angular/angular.js',
-            'bower_components/angular-route/angular-route.min.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'app/hello.js',
-            'test/unit/*.spec.js'
-
-        ],
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.min.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'app/hello.js',
+      'test/unit/*.spec.js'
+    ],
 
     // web server port
-        port: 9876,
+    port: 9876,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_WARN,
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_WARN,
 
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
-        browsers: [
-            'Chrome'
-        ],
+    browsers: [
+      // 'Chrome'
+      'PhantomJS'
+    ],
 
+    // Which plugins to enable
+    plugins: [
+      //  'karma-chrome-launcher',
+      'karma-phantomjs-launcher',
+      //'karma-coverage',
+      'karma-jasmine',
+      //'karma-jasmine-html-reporter'
+    ],
 
-         // Which plugins to enable
-         plugins: [
-           'karma-chrome-launcher',
-           //'karma-phantomjs-launcher',
-           //'karma-coverage',
-           'karma-jasmine',
-           //'karma-jasmine-html-reporter'
-         ],
-
-
-         // Continuous Integration mode
-         // if true, Karma captures browsers, runs the tests and exits
-         singleRun: false,
-
-        plugins: [
-           'karma-chrome-launcher',
-           'karma-jasmine',
-           //'karma-jasmine-html-reporter'
-        ],
-
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
   });
 };
